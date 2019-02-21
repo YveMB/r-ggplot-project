0cd142f7e964dba8b8193711db714cabb0d04fab
Yvette <User@YvetteMac.local>
2019-02-22 10:26:26 +1100

    shows the points more slearly with jitter

--- ggplotbasic.R
+++ ggplotbasic.R
@@ -5,9 +5,14 @@ interviews_plotting<-read_csv("data_output/interviews_plotting.csv")
 ##ggplot - this just tells ggplot what data we want to use
 ## aes - add aesthetics
 ## geom - adds geometry. point - scatter plot
+## (alpha=0.5) - makes some points lighter 
+## geom_jitter makes them move a little so they can be seen better 
 ggplot(data=interviews_plotting, 
        aes(x=no_membrs, y=number_items)) +
+  geom_jitter(alpha=0.5)
+
+## could also do like this 
+interview_plot +
   geom_point()
-  
 
 
