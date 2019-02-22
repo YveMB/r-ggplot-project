5c03ce1bae0246b55379cd5fae860012f7be1cf8
Yvette <User@YvetteMac.local>
2019-02-22 11:31:06 +1100

    more detailed plot

--- ggplotbasic.R
+++ ggplotbasic.R
@@ -14,8 +14,16 @@ interview_plot +
 ## can use the width and height arguements to specify the jitter
 interview_plot<- ggplot(data=interviews_plotting, 
        aes(x=no_membrs, y=number_items)) +
-  geom_jitter(alpha=0.5, width=0.15, height=0.15)
+  geom_jitter(alpha=0.5, width=0.15, height=0.15, size=4)
 ggsave("fig_output/membrs_item.png", interview_plot, width=15, height=10, 
        dpi=72)
 
-
+##to see on screen and have the villages id's by colour - if you put the color 
+##spec in the ggplot aes it applies to all - if you do it in jitter it only applies to the 
+## geomjitter commmand
+interview_plot_village<-ggplot(data=interviews_plotting, 
+       aes(x=no_membrs, y=number_items, color=village)) +
+  geom_jitter(alpha=0.5, width=0.15, height=0.15, size=2)+
+  geom_smooth(method="lm")
+ggsave("fig_output/membrs_item.png", interview_plot_village, width=15, height=10, 
+       dpi=72)
